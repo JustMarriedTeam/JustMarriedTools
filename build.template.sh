17d80c6131ea2c22c73b341e59db6d27ae98227e
kbhit <gurgul.grzegorz@gmail.com>
2017-05-25 17:59:15 +0200

    Improving performance of building, reducing complexity

--- build.template.sh
+++ build.template.sh
@@ -17,28 +17,20 @@ if [ -z "${COMPONENT_NAME}" ];
 then exit_with_error "Component name (COMPONENT_NAME) must be specified."
 fi
 
-
-ENVIRONMENT=${ENVIRONMENT:-production}
-VERSION=${VERSION:-latest}
-
-BUILD_NAME=${ENVIRONMENT}-${VERSION}
-BUILDER_IMAGE_NAME=${COMPONENT_NAME}-builder:${BUILD_NAME}
-APP_IMAGE_NAME=just-married/${COMPONENT_NAME}:${BUILD_NAME}
-
+BUILDER_IMAGE_NAME=${COMPONENT_NAME}-builder
+APP_IMAGE_NAME=justmarried/${COMPONENT_NAME}
 BUILD_ENVIRONMENT=${BUILD_ENVIRONMENT:-}
 
 mkdir -p artifacts
 
 print_header "STARTING BUILD (IMAGE ${APP_IMAGE_NAME})"
 
-print_header "BUILD STEP 1 (Building container for building ${BUILD_NAME})"
+print_header "BUILD STEP 1 (Building container for building ${APP_IMAGE_NAME})"
 
 docker build \
   -f containers/build/Dockerfile \
-  --build-arg ENVIRONMENT=${ENVIRONMENT} \
-  --build-arg VERSION=${VERSION} \
   -t ${BUILDER_IMAGE_NAME} . \
-  || exit_with_error "Could not build container for building ${BUILD_NAME}"
+  || exit_with_error "Could not build container for building ${APP_IMAGE_NAME}"
 
 
 
